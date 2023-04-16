6430a85c8a80e2176792f82f20c075fa59a61282
theghostintech <theghostintech@gmail.com>
2023-04-16 11:15:49 -0700

    Initial Commit

---
+++ BasicAgencyApp/BasicAgencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAgencyAppApp.swift
+//  BasicAgencyApp
+//
+//  Created by Joshua Birdine on 4/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BasicAgencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
